868295969dd05d9a5c587f152cb53c9b2f172a8f
dhalianeza <dhalianeza02@gmail.com>
2020-12-22 12:12:53 +0200

    modified

--- pictures/urls.py
+++ pictures/urls.py
@@ -3,11 +3,11 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = 'pictures'
+#app_name = 'pictures'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^search/', views.search_results, name='search'),
+    url(r'^search/', views.search_results, name='searchresults'),
     url(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
 ]
 if settings.DEBUG:
